e1036b68a49d4585ec1b4d0b7832a59110ab8be7
mcflugen <mcflugen@gmail.com>
2024-10-27 14:34:23 +0000

    add build and install sessions

--- noxfile.py
+++ noxfile.py
@@ -9,9 +9,37 @@ ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
 @nox.session
+def build(session: nox.Session) -> None:
+    """Build sdist and wheel dists."""
+    session.install("pip", "build")
+    session.install("setuptools")
+    session.run("python", "--version")
+    session.run("pip", "--version")
+    session.run("python", "-m", "build")
+
+
+@nox.session
+def install(session: nox.Session) -> None:
+    first_arg = session.posargs[0] if session.posargs else None
+
+    if first_arg:
+        if os.path.isfile(first_arg):
+            session.install(first_arg)
+        elif os.path.isdir(first_arg):
+            session.install(
+                "sensible_bmi", f"--find-links={first_arg}", "--no-deps", "--no-index"
+            )
+        else:
+            session.error("path must be a source distribution or folder")
+    else:
+        session.install(".")
+
+
+@nox.session
 def test(session: nox.Session) -> None:
     """Run the tests."""
-    session.install(".[testing]")
+    install(session)
+    session.install("-r", "requirements-testing.in", "-r", "requirements.in")
 
     session.run(
         "coverage",

1130eaebd60c711239b58e24ea9172881c6d3223
mcflugen <mcflugen@gmail.com>
2026-04-06 21:36:54 -0600

    manage version in pyproject.toml

--- src/sensible_bmi/_version.py
+++ src/sensible_bmi/_version.py
@@ -1,3 +1,9 @@
 from __future__ import annotations
 
-__version__ = "0.1.0"
+from importlib.metadata import PackageNotFoundError
+from importlib.metadata import version
+
+try:
+    __version__ = version("sensible-bmi")
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
